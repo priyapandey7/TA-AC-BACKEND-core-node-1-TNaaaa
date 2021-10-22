f23607601f159f99eb0507e75a8cc0554df2898b
priya pandey <priyarock746@gmail.com>
2021-10-22 23:54:44 +0530

    Assignment

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -1,12 +1,23 @@
 // File System Use
-let fs = require('fs');
-fs.readFile('./content.md', (err, content) => {
-  console.log(content.toString());
-});  
+// let fs = require('fs');
+// fs.readFile('./content.md', (err, content) => {
+//   console.log(content.toString());
+// });  
 // This code will run Asynchronously.
+
+
+// 
+var { readFile,readFileSync } = require('fs');
+readFile('./content.md','utf8',(err , content) => {
+  console.log(content);
+});
+
+var result =  readFileSync ('./content.md','utf8')
+  console.log(result);
 
 
 // Buffer
 let buff1 = Buffer.alloc(10);
 buff1.write('Welcome to Buffer');
 console.log(buff1.toString());
+
