28b9b95646dd9360000d93e44c479f32596ce160
priya pandey <priyarock746@gmail.com>
2021-10-26 07:10:12 +0530

    Assignmet

--- block-BNaaai/index.js
+++ block-BNaaai/index.js
@@ -1,8 +1,12 @@
 var urlString = "https://blog.altcampus.io:80/students/register?name=altcampus&gender=male"
 
 var url = require("url");
-const parsedUrl = url.parse(
-  "https://blog.altcampus.io:80/students/register?name=altcampus&gender=male",
-  true
-);
-console.log(parsedUrl);
+var parsedurl = url.parse('https://blog.altcampus.io:80/students/register?name=altcampus&gender=male',true)
+console.log(parsedurl);
+
+console.log( parsedUrl.pathname);
+console.log(parsedUrl.query);
+
+console.log(parsedUrl.host);
+console.log(parsedUrl.protocol);
+
