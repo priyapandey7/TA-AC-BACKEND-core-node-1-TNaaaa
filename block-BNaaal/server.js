c9be9c6d79726c1133b09d6d06de7c5eb5f77065
priya pandey <priyarock746@gmail.com>
2021-10-26 20:05:22 +0530

    Assignment

--- block-BNaaal/server.js
+++ block-BNaaal/server.js
@@ -1,7 +1,17 @@
+// var http = require('http');
+
+// http.createServer((request,response) =>{
+// console.log(request,response);
+// response.end('Welcome');
+// }).listen(4000,'localhost');
+
 var http = require('http');
 
-http.createServer((request,response) =>{
-console.log(request,response);
-response.end('Welcome');
-}).listen(4000,'localhost');
+var server = http.createServer(handleRequest);
 
+function handleRequest(request,response){
+    response.end('welcome')
+}
+server.listen (4000 , () =>{
+ console.log('server is listning on port');
+});
